282ad5168e0666b93c669881e6bda8c0196baab7
Rameshwar Singh <112180957+rameshwar-git@users.noreply.github.com>
2025-05-02 16:08:40 +0530

    Add files via upload
    02/05/2025

---
+++ 02-05-2025/regestration.js
@@ -0,0 +1,13 @@
+var name=document.getElementById("name");
+var fname=document.getElementById("fname");
+var address=document.getElementById("address");
+var state=document.getElementById("state");
+var pin=document.getElementById("pin");
+var email=document.getElementById("email");
+var cont=document.getElementById("contact");
+var reset=document.getElementById("reset");
+var reg=document.getElementById("submit");
+
+function submit(){
+    console.log(name.values);
+}
